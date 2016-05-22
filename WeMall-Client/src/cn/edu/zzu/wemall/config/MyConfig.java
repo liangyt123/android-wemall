6f2de4c0b4cb4385291e2602310cad7089c0c4fd
ChenJH <1035998209@qq.com>
2016-05-22 10:52:54 +0800

    修改某些布局的actionbar

--- WeMall-Client/src/cn/edu/zzu/wemall/config/MyConfig.java
+++ WeMall-Client/src/cn/edu/zzu/wemall/config/MyConfig.java
@@ -7,8 +7,7 @@ package cn.edu.zzu.wemall.config;
  */
 public class MyConfig {
 	// 此处配置服务器地址注意需要斜杠，一般情况下您只需要修改下面的这一个地址///////////////////////////////////////////////////////////////////////
-	//public static final String SERVERADDRESSBASE= "http://www.uaide.net/wemall/" ;
-	public static final String SERVERADDRESSBASE= "http://1490l010r6.imwork.net/wemall/" ;
+	public static final String SERVERADDRESSBASE= "http://www.uaide.net/wemall/" ;
 	//////////////////////////////////////////接口文件/////////////////////////////////////////////////////
 	public static final String SERVERADDRESS= SERVERADDRESSBASE+"/Api/client.php" ;
 	////此处配置User_Agent,服务端接口与此处要保持一致,否则服务器拒绝响应请求////////////////////////////////////
